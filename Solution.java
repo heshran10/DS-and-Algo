6d8c9d112235c260a28a5cae2955229b23c2f06e
hershan10 <22A91A05A0@aec.edu.in>
2023-08-19 22:24:00 +0530

    Add Solution.java for project progress

--- Solution.java
+++ Solution.java
@@ -1,49 +1,55 @@
 class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        if(matrix.length == 0) {
-            return false;
+    public int minimizedMaximum(int n, int[] quantities) {
+        
+        int start = 1; // to avoid divide by zero error
+        int end = Integer.MIN_VALUE; // for finding the max value.
+
+        for(int i=0;i<quantities.length;i++){
+            if(quantities[i]>end){
+                end = quantities[i];
+            }
         }
-        int m = matrix.length; // rows
-        int n = matrix[0].length; // col
-        int start = 0;
-        int end = m*n-1;
+
+        int res = -1;
 
         while(start<=end){
-            int mid = start+(end-start)/2;
-            int r = mid/n;
-            int c = mid%n;
-            int val = matrix[r][c];
-            if(val == target) {
-                return true;
-            } else if(target<val){
-                end = mid-1; 
+            int mid = start + (end-start)/2;
+            if(isDistributionPossible(quantities, mid, n)){
+                res = mid;
+                end = mid-1;
             } else {
                 start = mid+1;
             }
         }
-        return false;
+        return res;
     }
-
-   public boolean searchMatrix2(int[][] matrix, int target) {
-        if(matrix.length==0){
-            return false;
-        }
-        int Rows = matrix.length;
-        int Cols = matrix[0].length;
-        int r = 0;
-        int c = Cols-1;
-        while(r < Rows && c >=0) {
-            if(matrix[r][c] == target) {
-                return true;
-            } else if(target < matrix[r][c]){
-                c = c-1;
-            } else {
-                r = r+1;
+    boolean isDistributionPossible(int quantities[], int maxProducts, int stores) {
+        int storeCount = 0;
+        for(int i=0;i<quantities.length;i++){
+            storeCount += quantities[i]/maxProducts;
+            if(quantities[i]%maxProducts!=0){
+                storeCount+=1;
+            }
+            if(storeCount>stores) {
+                return false;
             }
         }
-        return false;
-        
-    }
+        return true;
 
+    }
 }
-// Commit timestamp: 2025-07-18 22:42:00
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Commit timestamp: 2023-08-19 22:24:00
