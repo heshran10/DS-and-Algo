fede211cf3c22eccd9d82496df59b7643763d195
hershan10 <22A91A05A0@aec.edu.in>
2023-11-06 15:38:00 +0530

    Add MenMed.java for project progress

--- MenMed.java
+++ MenMed.java
@@ -44,4 +44,4 @@ public class MenMed {
 // this is one of the easy one okkey harsh
 // i have to just remenber the past ones
 // for the realizations
-// Commit timestamp: 2025-06-07 16:42:00
+// Commit timestamp: 2023-11-06 15:38:00
