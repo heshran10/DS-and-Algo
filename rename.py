aeeb5806155fef6d3e54251a125c4a98b3cc4d7c
hershan10 <22A91A05A0@aec.edu.in>
2023-11-09 15:40:00 +0530

    Add rename.py for project progress

--- rename.py
+++ rename.py
@@ -27,4 +27,4 @@ def rename_folders(base_path):
                 print(f"Renamed: {folder} -> {new_name}")
 rename_folders(base_path)
 
-// Commit timestamp: 2023-07-18 20:11:00
+// Commit timestamp: 2023-11-09 15:40:00
