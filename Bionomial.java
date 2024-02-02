0cd2fb674b8636f6e24dcb070121ed3c64c6dc52
hershan10 <22A91A05A0@aec.edu.in>
2024-02-02 08:20:00 +0530

    Add Bionomial.java for project progress

--- Bionomial.java
+++ Bionomial.java
@@ -29,4 +29,4 @@ public class Bionomial {
 // then bionomial function which includes the para
 // in main function we just call what we want
 // simple
-// Commit timestamp: 2023-10-06 14:29:00
+// Commit timestamp: 2024-02-02 08:20:00
