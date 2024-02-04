b23e6fa212fe18bb93ba8d4acdfbc58f5a7f33b0
hershan10 <22A91A05A0@aec.edu.in>
2024-02-04 09:16:00 +0530

    Add InputFuc.java for project progress

--- InputFuc.java
+++ InputFuc.java
@@ -39,4 +39,4 @@ public class InputFuc {
 // scope 2 has inner parameter which can return
 // and in main function we take input and just call
 // the function         
-// Commit timestamp: 2023-10-08 20:20:00
+// Commit timestamp: 2024-02-04 09:16:00
