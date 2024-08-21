f0e682e30e3f79b26051b152ec24dc8bdc1f1e48
hershan10 <22A91A05A0@aec.edu.in>
2024-08-21 18:16:00 +0530

    Add MyStudent.java for project progress

---
+++ MyStudent.java
@@ -0,0 +1,11 @@
+package com.college.student;
+public class MyStudent {
+    public static void main(String[] args) {
+        MyStudent ms = new MyStudent();
+        ms.print();
+    }
+    private void print(){
+        System.out.println("I am Student");
+    }
+}
+// Commit timestamp: 2024-08-21 18:16:00
