1a211ae1801bd631517dd6e82d5dcd2ff060f324
hershan10 <22A91A05A0@aec.edu.in>
2024-08-23 20:41:00 +0530

    Add StudentDetails.java for project progress

---
+++ StudentDetails.java
@@ -0,0 +1,7 @@
+package com.college.student.studentdetails;
+public class StudentDetails {
+    public void print(){
+        System.out.println("I am StudentDetails");
+    }
+}
+// Commit timestamp: 2024-08-23 20:41:00
