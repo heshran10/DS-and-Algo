f10aa9724cf023bf52f04329b88b3ec92d2e3905
hershan10 <22A91A05A0@aec.edu.in>
2024-08-24 11:38:00 +0530

    Add TeacherDetails.java for project progress

---
+++ TeacherDetails.java
@@ -0,0 +1,7 @@
+package com.college.teacher.teacherdetails;
+public class TeacherDetails {
+    public void print(){
+        System.out.println("I am TeacherDetails");
+    }
+}
+// Commit timestamp: 2024-08-24 11:38:00
