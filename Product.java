f2f401c2fb41d2810e9c04618f453946c4cddbcf
hershan10 <22A91A05A0@aec.edu.in>
2024-09-20 17:37:00 +0530

    Add Product.java for project progress

--- Product.java
+++ Product.java
@@ -28,4 +28,4 @@ public class Product {
 //add the numbers in it and it gaves the value
 // IN int,float it return value which can you use in the
 //next main function
-// Commit timestamp: 2024-05-29 20:35:00
+// Commit timestamp: 2024-09-20 17:37:00
