08995a7ed24d6c614276e87526d6803bb4e4f975
hershan10 <22A91A05A0@aec.edu.in>
2024-11-06 19:24:00 +0530

    Add ReverseAWordInString.java for project progress

---
+++ ReverseAWordInString.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String reverseWords(String s) {
+        StringBuilder res = new StringBuilder();
+        int startIndex = s.length()-1;
+        while (startIndex>=0){
+            while ( startIndex>=0 && s.charAt(startIndex)==' '){
+                startIndex--;
+            }
+            if(startIndex<0){
+                break;
+            }
+            int endIndex = startIndex;
+            while ( startIndex>=0 && s.charAt(startIndex)!=' '){
+                startIndex--;
+            }
+            // word
+            if(res.length()==0){
+                res.append(s.substring(startIndex+1,endIndex+1));
+            } else {
+                res.append(" ");
+                res.append(s.substring(startIndex+1,endIndex+1));
+            }
+        }
+        return res.toString();
+    }
+}
+// Commit timestamp: 2024-11-06 19:24:00
