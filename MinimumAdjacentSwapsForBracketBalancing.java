0a93c6137ff774351b63614aa69593629d10173f
hershan10 <22A91A05A0@aec.edu.in>
2024-11-15 19:00:00 +0530

    Add MinimumAdjacentSwapsForBracketBalancing.java for project progress

---
+++ MinimumAdjacentSwapsForBracketBalancing.java
@@ -0,0 +1,21 @@
+class Solution{
+    static int minimumNumberOfSwaps(String s){
+        // code here
+        int open=0,close=0,unbalanced=0,swaps=0;
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch == '[') {
+                open++;
+                if(unbalanced>0){
+                    swaps += unbalanced;
+                    unbalanced--;
+                }
+            } else {
+                close++;
+                unbalanced = close-open;
+            }
+        }
+        return swaps;
+    }
+}
+// Commit timestamp: 2024-11-15 19:00:00
