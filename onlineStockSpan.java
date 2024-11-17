94cfabdc6191f52aa96b399e1f9d90dddafed411
hershan10 <22A91A05A0@aec.edu.in>
2024-11-17 22:01:00 +0530

    Add onlineStockSpan.java for project progress

---
+++ onlineStockSpan.java
@@ -0,0 +1,24 @@
+class StockSpanner {
+
+    Stack<Integer> stack;
+    ArrayList<Integer> list;
+    public StockSpanner() {
+         stack = new Stack<>();
+         list = new ArrayList<>();
+    }
+    
+    public int next(int price) {
+        list.add(price);
+        // index = stack.peek()
+        // list.get(index) -> value
+        while(!stack.isEmpty() && list.get(stack.peek())<=price){
+            stack.pop();
+        }
+        int previousGreaterIndex = (stack.isEmpty())?-1:stack.peek();
+        int currentIndex = list.size()-1;
+        int ans = currentIndex - previousGreaterIndex;
+        stack.push(currentIndex);
+        return ans;
+    }
+}
+// Commit timestamp: 2024-11-17 22:01:00
