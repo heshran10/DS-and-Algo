49eb0e4644e4b4cd0cebd8a8432ee64eef8ecddb
hershan10 <22A91A05A0@aec.edu.in>
2025-01-15 13:20:00 +0530

    Add helloWorld.java for project progress

---
+++ helloWorld.java
@@ -0,0 +1,7 @@
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("helloWorld");
+    }
+}
+// for the hello world
+// Commit timestamp: 2025-01-15 13:20:00
