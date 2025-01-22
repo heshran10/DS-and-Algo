2bc90ad7ca19793db3632527f36cc342edbdeaa2
hershan10 <22A91A05A0@aec.edu.in>
2025-01-22 09:02:00 +0530

    Add largeIntwo.java for project progress

---
+++ largeIntwo.java
@@ -0,0 +1,18 @@
+package Importrant;
+
+public class largeIntwo {
+    public static void main(String[] args) {
+        int one = 18;
+        int two = 20;
+        if (one >= two) {
+        System.out.println("one is bigger ");
+            
+        }else{
+            System.out.println("two is big");
+        }
+        
+    }
+}
+
+// just simple if conditions is apply bro
+// Commit timestamp: 2025-01-22 09:02:00
