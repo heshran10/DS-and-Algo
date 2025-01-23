d0a2394dc66899abc27dd7076c64e1226899f13a
hershan10 <22A91A05A0@aec.edu.in>
2025-01-23 09:19:00 +0530

    Add StrtOne.java for project progress

---
+++ StrtOne.java
@@ -0,0 +1,33 @@
+// package Importrant;
+
+// public class StrtOne {
+//     public static void main(String[] args) {
+//         int age = 18;
+//         if(age>=18){
+//             System.out.println("you are adult");
+//         }
+//         else{
+//             System.out.println("not adult");
+//         }
+//     }
+// }
+
+// This is fierst conditions
+
+
+// public class StrtOne {
+//     public static void main(String[] args) {
+//         int age = 18;
+//         if(age>=18){
+//             System.out.println("you are adult");
+//         }
+//         if (age> 13 && age<17) 
+//         {
+//         System.out.println(" you are nat mid age");    
+//         }
+//         else{
+//             System.out.println("not adult");
+//         }
+//     }
+// }
+// Commit timestamp: 2025-01-23 09:19:00
