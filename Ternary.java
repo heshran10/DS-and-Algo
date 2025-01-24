f7a9f74366e97ebbd870335406320586478b014f
hershan10 <22A91A05A0@aec.edu.in>
2025-01-24 13:35:00 +0530

    Add Ternary.java for project progress

---
+++ Ternary.java
@@ -0,0 +1,23 @@
+package Importrant;
+
+public class Ternary {
+    public static void main(String[] args) {
+        
+    int number = 4;
+    
+    String type = ((number%2) ==0)? "even": "odd";
+    System.out.println(type);
+
+
+    int pass = 33;
+
+    String result = (pass>=33)? "pass":"fail";
+    System.out.println(result);
+    }
+
+
+
+    
+}
+
+// Commit timestamp: 2025-01-24 13:35:00
