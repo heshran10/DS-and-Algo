42542989928b10cf39031a2df469a92205257e1d
hershan10 <22A91A05A0@aec.edu.in>
2025-02-02 12:08:00 +0530

    Add Bionomial.java for project progress

---
+++ Bionomial.java
@@ -0,0 +1,32 @@
+public class Bionomial {
+
+    
+    public static int factorial(int n){
+        int f = 1;
+        for(int i = 1; i<=n ; i++){
+            f = f*i;
+        }
+        return f;
+    }
+
+    public static int bionomial(int n , int r){
+        int nf = factorial(n);
+        int rf = factorial(r);
+        int nmrf = factorial(n-r);
+
+        int bio = nf / (rf*nmrf);
+
+        return bio;
+    }
+    public static void main(String[] args) {
+        System.out.println(bionomial(3, 2));
+    }
+}
+
+
+
+// so basicall first in factorial function
+// then bionomial function which includes the para
+// in main function we just call what we want
+// simple
+// Commit timestamp: 2025-02-02 12:08:00
