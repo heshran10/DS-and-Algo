228f9bf21134972497c78f92545a8f8bb887430c
hershan10 <22A91A05A0@aec.edu.in>
2025-02-05 09:00:00 +0530

    Add Hello.java for project progress

---
+++ Hello.java
@@ -0,0 +1,15 @@
+public class Hello {
+
+    public static void HelloWorld(){
+        System.out.println("hello wrold");
+    }
+    public static void main(String[] args) {
+        HelloWorld(); // function class
+    }
+}
+//  So basically function is depends on the
+// two things name , call by(), retur type
+// if void so it cant 
+// but if int,flot so retunrn(n)
+
+// Commit timestamp: 2025-02-05 09:00:00
