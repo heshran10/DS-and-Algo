7fe42f4df67d3f19706f2997ad3401a9ede425f1
hershan10 <22A91A05A0@aec.edu.in>
2025-02-08 08:34:00 +0530

    Add Primerange.java for project progress

---
+++ Primerange.java
@@ -0,0 +1,32 @@
+public class Primerange {
+     
+    public static boolean isPrime(int n){
+
+        if (n==2) {
+            return true;
+        }
+        boolean isPrime = true;
+        for(int i =2; i<=n ; i++){
+            if (n % i == 0) {
+                isPrime = false;
+                break;
+            }
+    }
+    return isPrime;
+    }
+
+    public static void primesInRange(int n ){
+        for(int i= 2; i<=n; i++){
+            if (isPrime(i)) {
+                System.out.println(i+"");
+                
+            }
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+            primesInRange(100);
+    }
+}
+
+// Commit timestamp: 2025-02-08 08:34:00
