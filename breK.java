53f882183293fdc9c56e0c1751d0b99cc9fb5647
hershan10 <22A91A05A0@aec.edu.in>
2025-02-11 22:06:00 +0530

    Add breK.java for project progress

---
+++ breK.java
@@ -0,0 +1,15 @@
+package Loops;
+
+public class breK {
+    public static void main(String[] args) {
+        // for the break statement
+        for(int i =0; i<=10; i++){
+            if(i==3){
+                break;
+            }
+            System.out.println("i am out");
+        }
+    }
+}
+
+// Commit timestamp: 2025-02-11 22:06:00
