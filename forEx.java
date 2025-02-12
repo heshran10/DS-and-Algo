e581108a54f3eebbb977fe0731e74277f3ecb659
hershan10 <22A91A05A0@aec.edu.in>
2025-02-12 15:40:00 +0530

    Add forEx.java for project progress

---
+++ forEx.java
@@ -0,0 +1,15 @@
+
+
+public class forEx {
+    public static void main(String[] args) {
+        // this is the eample of for loop
+        // in this counter initialzation and updation all done in same line
+        for(int i =0; i<=10 ;i++){
+            System.out.println( " hello harsh 10 times");
+        }
+    }
+}
+
+
+// ; yevala aata he isme ok
+// Commit timestamp: 2025-02-12 15:40:00
