557c5b43439511657db549c7aeb454551acad59e
hershan10 <22A91A05A0@aec.edu.in>
2025-02-13 16:04:00 +0530

    Add forOne.java for project progress

---
+++ forOne.java
@@ -0,0 +1,15 @@
+package Loops;
+
+public class forOne {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i<=10) {
+            System.out.println(i);
+        i++;
+        }
+        
+    }
+}
+// simple
+
+// Commit timestamp: 2025-02-13 16:04:00
