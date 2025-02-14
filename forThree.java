9d5eee225503c0fc47207f6bc4be769d8ad17247
hershan10 <22A91A05A0@aec.edu.in>
2025-02-14 10:44:00 +0530

    Add forThree.java for project progress

---
+++ forThree.java
@@ -0,0 +1,20 @@
+
+
+import java.util.Scanner;
+
+public class forThree {
+    public static void main(String[] args) {
+     Scanner sc = new Scanner(System.in);
+     int n = sc.nextInt();
+     int sum = 0;
+
+     int i =1;
+     while (i<=n) {
+        sum += i;
+        i++;
+     }
+     System.out.println(sum);
+    }
+}
+
+// Commit timestamp: 2025-02-14 10:44:00
