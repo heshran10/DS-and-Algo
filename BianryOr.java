c690a8af04f571ff6b058c08765741a3582951c4
hershan10 <22A91A05A0@aec.edu.in>
2025-02-15 19:56:00 +0530

    Add BianryOr.java for project progress

---
+++ BianryOr.java
@@ -0,0 +1,15 @@
+package BitManipulation;
+
+// in this the example of or (|)
+// menas decimal convert in binary and binary checcks the or condition and get the result
+// only both 0 cant accepted here
+
+
+public class BianryOr {
+    public static void main(String[] args) {
+        System.out.println(5|6);
+    }
+}
+
+// the output is 7
+// Commit timestamp: 2025-02-15 19:56:00
