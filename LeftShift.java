41ffd328fdefd961ef3b9a79f5782108e095d302
hershan10 <22A91A05A0@aec.edu.in>
2025-02-17 12:51:00 +0530

    Add LeftShift.java for project progress

---
+++ LeftShift.java
@@ -0,0 +1,17 @@
+package BitManipulation;
+
+//leftshift is looks as <<
+// 5<<2
+// 5 means the bit of 5 and 2 is how much chnage in that side
+// convert into bits and shift bits that side and fill the remain with the 0
+
+public class LeftShift {
+    public static void main(String[] args) {
+        System.out.println(8<<3);
+    }
+}
+
+
+// ans is 64
+// formula is a *b raise 2
+// Commit timestamp: 2025-02-17 12:51:00
