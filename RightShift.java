b9995ef59c978d99d67dd510a5f828041f9168b2
hershan10 <22A91A05A0@aec.edu.in>
2025-02-19 17:11:00 +0530

    Add RightShift.java for project progress

---
+++ RightShift.java
@@ -0,0 +1,18 @@
+package BitManipulation;
+
+
+//rightshift is looks as >>
+// 5>>2
+// 5 means the bit of 5 and 2 is how much change in that side
+// convert into bits and shift bits that side and fill the remain with the 0
+public class RightShift {
+    public static void main(String[] args) {
+        System.out.println(5>>6);
+    }
+}
+
+
+// ans is 0
+// formula a / b raise 2
+
+// Commit timestamp: 2025-02-19 17:11:00
