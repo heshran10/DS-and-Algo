a5c3fe8002cdcc00bebd69c0506acebfe7d82acb
hershan10 <22A91A05A0@aec.edu.in>
2025-02-21 15:48:00 +0530

    Add OddEven.java for project progress

---
+++ OddEven.java
@@ -0,0 +1,11 @@
+
+//check the oddeven by the bit
+// check with the operators.
+
+public class OddEven {
+    public static void main(String[] args) {
+        
+    }
+}
+
+// Commit timestamp: 2025-02-21 15:48:00
