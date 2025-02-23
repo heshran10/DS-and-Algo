897ef5046f17e628a01198c4bcfdd9c89a7d8cec
hershan10 <22A91A05A0@aec.edu.in>
2025-02-23 22:20:00 +0530

    Add Wow.java for project progress

---
+++ Wow.java
@@ -0,0 +1,18 @@
+package ese;
+
+public class Wow {
+
+
+    static String name1 = " hasrsh joshi ";
+
+    public static void namebta(String name ){
+    System.out.println(name);
+    }
+
+    public String name = "harsh 2";
+    public static void main(String[] args) {
+        namebta(name);
+    }
+}
+
+// Commit timestamp: 2025-02-23 22:20:00
