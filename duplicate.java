4e8f2dec102c5e93f597cb35f62f5d15d2641036
hershan10 <22A91A05A0@aec.edu.in>
2025-03-07 21:42:00 +0530

    Add duplicate.java for project progress

---
+++ duplicate.java
@@ -0,0 +1,19 @@
+public class duplicate {
+
+    public static boolean hasduplicate(int num[]){
+        for(int i=0; i<num.length;i++){
+            for (int j = i+1; j < num.length; j++) {
+                if (num[i] == num[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int num[] = {1,2,3,4,5};
+        System.out.println(hasduplicate(num));
+    }
+}
+
+// Commit timestamp: 2025-03-07 21:42:00
