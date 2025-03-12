16120189347b26fa75306f2f4578e708d47171ec
hershan10 <22A91A05A0@aec.edu.in>
2025-03-12 09:53:00 +0530

    Add secondmax.java for project progress

---
+++ secondmax.java
@@ -0,0 +1,39 @@
+package Questions.Arrays;
+// to find second max element in the program
+
+public class secondmax {
+
+    public static int secondmax(int[] nums){
+
+        if (nums.length <2) {
+            return Integer.MIN_VALUE;
+        }
+
+        int max = Integer.MIN_VALUE;
+        int secondmax = Integer.MIN_VALUE;
+      
+        for(int i =0; i<nums.length; i++){
+           if (nums[i] > max) {
+            secondmax = max;
+            max = nums[i];
+           }else if(nums[i] > secondmax && nums[i] != max){
+                    secondmax = nums[i];
+           }
+        }
+        return secondmax;
+    }
+
+
+
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6,7};
+
+        int secondmaxE = secondmax(nums);
+        System.out.println(secondmaxE);
+
+
+    }
+}
+
+// Commit timestamp: 2025-03-12 09:53:00
