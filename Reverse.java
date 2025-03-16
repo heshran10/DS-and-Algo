d23a02825c9c2f54592e493e580682804ead1bb9
hershan10 <22A91A05A0@aec.edu.in>
2025-03-16 21:19:00 +0530

    Add Reverse.java for project progress

---
+++ Reverse.java
@@ -0,0 +1,27 @@
+package Questions.Strings;
+
+public class Reverse {
+
+    public static String reverse(String str){
+        char[] charArray = str.toCharArray();
+        int left = 0;
+        int right =str.length() -1;
+
+
+        while (left<right) { 
+            char temp = charArray[left];
+            charArray[left] = charArray[right];
+            charArray[right] = temp;
+            left++;
+            right--;
+        }
+        return new String(charArray);
+    }
+    public static void main(String[] args) {
+        String str = "harsh";
+        String result = reverse(str);
+        System.out.println(result);
+    }
+}
+
+// Commit timestamp: 2025-03-16 21:19:00
