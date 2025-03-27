b312297c343d12796b24e10b3a527b4728ffa5b1
hershan10 <22A91A05A0@aec.edu.in>
2025-03-27 15:49:00 +0530

    Add inc.java for project progress

---
+++ inc.java
@@ -0,0 +1,28 @@
+package Recursion;
+
+// increment for n numbers in java
+
+public class inc {
+
+    public static void inc(int n) {
+        if(n==0){
+            System.out.println(n);
+            return;
+        }
+
+        inc(n-1);
+        System.out.println(n);
+    }
+    public static void main(String[] args) {
+        inc(12);
+    }
+}
+
+
+// logic
+
+// firstly incremental value 
+// go untill avalue to minus 1 and then print
+// reverse the loop
+
+// Commit timestamp: 2025-03-27 15:49:00
