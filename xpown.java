bb272bb50d11ed5d17fa8cb8ae464801afa9a3bc
hershan10 <22A91A05A0@aec.edu.in>
2025-04-05 18:12:00 +0530

    Add xpown.java for project progress

---
+++ xpown.java
@@ -0,0 +1,27 @@
+package Recursion;
+
+
+//print x to power n
+//x into xn-1; 
+
+
+public class xpown {
+
+
+    public static int power(int x, int n) {
+
+        if(n==0){
+            return 1;
+        }
+
+        int pow = power(x, n-1);
+        int fn = x * pow;
+        return fn;
+        
+    }
+    public static void main(String[] args) {
+        System.out.println(power(2, 2));
+    }
+}
+
+// Commit timestamp: 2025-04-05 18:12:00
