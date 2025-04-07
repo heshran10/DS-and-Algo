ba74f7e2e9233b42749e1d85c84a04f83161a326
hershan10 <22A91A05A0@aec.edu.in>
2025-04-07 19:16:00 +0530

    Add methods.java for project progress

---
+++ methods.java
@@ -0,0 +1,13 @@
+package Strings;
+
+public class methods {
+
+    public static void main(String[] args) {
+         String fullname = "HARSHJOSHI";
+    System.out.println(fullname.length());
+
+    }
+   
+}
+
+// Commit timestamp: 2025-04-07 19:16:00
