73e92c31290b60763acd2e8b955ab893b4ad7f22
hershan10 <22A91A05A0@aec.edu.in>
2025-04-08 21:04:00 +0530

    Add palindrome.java for project progress

---
+++ palindrome.java
@@ -0,0 +1,21 @@
+package Strings;
+
+public class palindrome {
+
+    public static boolean pali(String str) {
+        for(int i = 0; i <= str.length() / 2; i++) {
+            int n = str.length();
+            if(str.charAt(i) != str.charAt(n - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String str = "wow";
+        System.out.println(pali(str));
+    }
+}
+
+// Commit timestamp: 2025-04-08 21:04:00
