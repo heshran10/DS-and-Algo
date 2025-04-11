62474abfe1ff15304e60fc7bf23f20aed30b8b1b
hershan10 <22A91A05A0@aec.edu.in>
2025-04-11 16:28:00 +0530

    Add StringBui.java for project progress

---
+++ StringBui.java
@@ -0,0 +1,18 @@
+
+
+public class StringBui {
+    
+    public static void main(String[] args) {
+        
+        StringBuilder sb = new StringBuilder();
+
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            sb.append(ch);
+        }
+        System.out.println(sb);
+    }
+}
+
+
+
+// Commit timestamp: 2025-04-11 16:28:00
