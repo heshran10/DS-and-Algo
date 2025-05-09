349a41d1dcd8c6410675e3a19d5edf364262ebcd
hershan10 <22A91A05A0@aec.edu.in>
2025-05-09 13:14:00 +0530

    Add Swap.java for project progress

--- Swap.java
+++ Swap.java
@@ -24,4 +24,4 @@ public class Swap {
 // not fix arguments also used function instead or
 // para meter if we dwclare para in funtion but we want to add arguments 
 // its also usefull.
-// Commit timestamp: 2025-01-15 19:55:00
+// Commit timestamp: 2025-05-09 13:14:00
