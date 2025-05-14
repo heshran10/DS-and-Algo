bb3e474c006b887a4301268c36f45ece9f860435
hershan10 <22A91A05A0@aec.edu.in>
2025-05-14 13:05:00 +0530

    Add Binaryxor.java for project progress

--- Binaryxor.java
+++ Binaryxor.java
@@ -14,4 +14,4 @@ public class Binaryxor {
 
 // the output is 3
 //change in bits convert in into another same 0 different is 1
-// Commit timestamp: 2025-01-17 13:02:00
+// Commit timestamp: 2025-05-14 13:05:00
