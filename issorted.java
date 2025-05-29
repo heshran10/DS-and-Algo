723427631bf0113eaa9c135d9e70eae42d48578b
hershan10 <22A91A05A0@aec.edu.in>
2025-05-29 20:06:00 +0530

    Add issorted.java for project progress

--- issorted.java
+++ issorted.java
@@ -1,19 +1,33 @@
-public  class issorted {
+package Recursion;
 
 
-   public static boolean issort(int num[]){
-    for(int i=1; i<num.length; i++){
-        if(num[i] < num[i-1]){
+// check the given number is sorted or not;
+// array 
+// index 
+// and range
+
+
+public class issorted {
+
+    public static boolean check(int array[], int i) {
+
+        if(i==array.length-1){
+            return true;
+        }
+
+
+        if(array[i] >array[i+1]){
             return false;
         }
+        
+        return check(array, i+1);
+
+
     }
-    return true;
-   }
-
     public static void main(String[] args) {
-        int num[] = {1,5,3,4,5,6};
-        System.out.println(issort(num));
-
+        int array[]= {1,2,3,4,5,6,7};
+        System.out.println(check(array, 5));
     }
 }
-// Commit timestamp: 2025-05-22 11:27:00
+
+// Commit timestamp: 2025-05-29 20:06:00
